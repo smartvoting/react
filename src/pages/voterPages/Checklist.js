1bffe452809de3f8a57333dfc080de0065618379
[Insert Name Here] <michael.sirna@georgebrown.ca>
2022-02-14 11:28:50 -0500

    Over engineered checklist.js

--- src/pages/voterPages/Checklist.js
+++ src/pages/voterPages/Checklist.js
@@ -7,25 +7,87 @@ import img6 from '../../images/checklistImages/vote3.jpg';
 import img7 from '../../images/checklistImages/vote4.jpg';
 import img8 from '../../images/checklistImages/bluePC2.jpg';
 import img9 from '../../images/checklistImages/PinkID2.jpg';
-import { Card, Container, Button, Row, Col, } from "react-bootstrap";
+import img10 from '../../images/checklistImages/logoGreen.png';
+import ecLogo from '../../images/ecLogo.png';
+import { Card, CardGroup, Container, Row, Col, } from "react-bootstrap";
 
 export default function CheckList() {
+    const checklistArray = [
+        "<strong>Make sure you're registered</strong> at your current address",
+        "Watch for your <strong>voter information card</strong> in the mail",
+        "<strong>Make a plan to vote</strong>",
+        "Find out <strong>who the candidates are</strong> in your riding",
+        "<strong>Bring ID</strong> when you go to vote"];
+
     return (
         <Container>
+            <Container style={{ width:"100%", }}>
+                <Container className="downloadInfographic">
+                    <p>Download infographic in <a target="_blank" href="https://www.elections.ca/vot/vop/checklist-aide-memoire_e.pdf">PDF</a> | <a target="_blank" href="https://www.elections.ca/vot/vop/checklist-aide-memoire_e.jpg">JPG</a></p>
+                 </Container>
+            </Container>
+            <br/>
             <p className="text-center" style={{ fontSize: "18px" }}>Get to know your voting options and choose what works best for you.<br />No matter how you vote, keeping you healthy and safe is our priority.</p>
             <p className="text-center" style={{ fontSize: "18px", fontWeight: "bold", }}>If you <a className="voteByMail" href="/content.aspx?section=vot&amp;dir=reg/svr&amp;document=index&amp;lang=e">plan to vote by mail</a>, don't wait, apply now at elections.ca.</p>
             <br/>
             <Container>
-                <Row style={{alignItems:"center",}}>
-                    <Col sm={4}><img style={{ width:"100%", }} src={img1}/></Col>
-                    <Col sm={8} style={{display:"flex"}}>
-                        <Container style={{border:"solid 2px black", backgroundColor:"white", width:"5%", marginLeft:"10px", marginRight:"20px"}}></Container>
-                        <p style={{ fontSize: "20px", margin:"5px 0 5px 0" }}>Make sure you're registered at your current address</p>
-                    </Col>
-                </Row>
+                {Array.from({ length: checklistArray.length }).map((_, idx) => (
+                    ChecklistItem(idx, checklistArray[idx])
+                ))}
             </Container>
+            <br />
+            <hr />
+            <img style={{ display: "flex", margin: "auto", }} src={img10} />
+            <br />
+            <Row style={{ flexDirection: "row" }}>
+                <Col sm={8} style={{ fontSize: "18px" }}>
+                    <p style={{ fontWeight: "bold" }}>Visit elections.ca for the official information on voting and the health and safety measures in place.</p>
+                    <p>1-800-463-6868 / <strong>elections.ca</strong> / TTY1-800-361-8935</p>
+                </Col>
+                <Col sm={4}>
+                    <img style={{ width: "100%", }} src={ecLogo} />
+                </Col>
+            </Row>
 
         </Container>
+    );
+}
+
+function ChecklistItem(index, text) {
+    const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9,]
+    const cardGroupText = ["At your assigned polling station<strong> on election day</strong>",
+        "At your assigned polling station<strong> on advance polling days</strong>",
+        "<strong>By mail:</strong> visit <strong>elections.ca</strong> to see if voting by mail is the right choice for you (deadlines apply)",
+        "At any <strong>Elections Canada office</strong> across Canada (deadlines apply)"];
+
+
+    return (
+        <Row key={index} style={index % 2 !== 1 ? { backgroundColor: "#3333", alignItems: "center", padding: "10px 0 10px 0" } : { alignItems: "center", padding: "10px 0 10px 0" }}>
+            <Col sm={4}><img style={{ width: "100%", }} src={index >= 3 ? images[index + 4] : images[index]} /></Col>
+            <Col sm={8} style={{ display: "flex" }}>
+                <Container style={{ border: "solid 4px black", backgroundColor: "white", width: "1%", marginLeft: "10px", marginRight: "20px" }}></Container>
+                <p id="itemText" style={{ fontSize: "20px", margin: "10px 0 10px 0" }} dangerouslySetInnerHTML={{ __html: text }}></p>
+            </Col>
+            {index === 2 ?
+                <CardGroup>
+                    {Array.from({ length: cardGroupText.length }).map((_, idx) => (
+                        CreateCardGroupItem(idx, images[idx + 3], cardGroupText[idx])
+                    ))}
+                </CardGroup> :
+                null
+            }
+        </Row>
+    );
+}
+
+function CreateCardGroupItem(index, img, text) {
+    return (
+        <Card key={index}>
+            <Card.Img variant="top" src={img} />
+            <Card.Body>
+                <Card.Text dangerouslySetInnerHTML={{ __html: text }}></Card.Text>
+            </Card.Body>
+        </Card>
     );
 }
 
